95646cbcf973b346c2835a329bcea1cbc0a27dd9
exequielraineri <exeraineriuni@gmail.com>
2023-11-30 11:00:12 -0300

    Mejore visual

--- src/componentes/TaskItem.jsx
+++ src/componentes/TaskItem.jsx
@@ -22,47 +22,75 @@ export default function TaskItem({ item, actualizar, eliminar, estado }) {
     }
 
     return (
-      <form className="todoActualizarForm" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          className="todoInput"
-          value={newValue}
-          onChange={handlerChange}
-        />
-        <input
-          type="submit"
-          value="Actualizar"
-          className="btn btnActualizar"
-          onClick={handleClickActualizar}
-        />
-      </form>
+      <>
+        <cite
+          className={
+            item.completed ? "estado-task listo" : "estado-task pendiente"
+          }
+        >
+          {item.completed ? "Completado" : "Pendiente"}
+        </cite>
+        <form
+          className="todoActualizarForm"
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="text"
+            className="todoInput"
+            value={newValue}
+            onChange={handlerChange}
+          />
+          <input
+            type="submit"
+            value="Actualizar"
+            className="btn btnActualizar"
+            onClick={handleClickActualizar}
+          />
+        </form>
+      </>
     );
   }
 
   function TodoElement() {
     return (
-      <div className="todo-item">
+      <>
+        <cite
+          className={
+            item.completed ? "estado-task listo" : "estado-task pendiente"
+          }
+        >
+          {item.completed ? "Completado" : "Pendiente"}
+        </cite>
+        <div className="todo-item">
           {item.completed ? (
             <div className="completado">
               <span>✔</span>
             </div>
           ) : (
             <div className="incompleto">
-            <input type="checkbox" onClick={() => estado(item.id)}></input>
+              <input
+                type="checkbox"
+                onClick={() => estado(item.id)}
+              ></input>
             </div>
           )}
- 
- 
 
-              {item.title}
-           
-        <button className="btn btnEditar" onClick={() => setIsEdit(true)}>
-          Editar
-        </button>
-        <button className="btn btnEliminar" onClick={() => eliminar(item.id)}>
-          Eliminar
-        </button>
-      </div>
+          <h5>{item.title}</h5>
+
+          <button
+            className="btn btnEditar"
+            onClick={() => setIsEdit(true)}
+          >
+            Editar
+          </button>
+          <button
+            className="btn btnEliminar"
+            onClick={() => eliminar(item.id)}
+          >
+            Eliminar
+          </button>
+        </div>
+      </>
     );
   }
 
